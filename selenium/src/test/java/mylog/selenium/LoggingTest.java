2b233ca29e5a7275625298f16f278fd378aa60fb
Zackery <Zackerysmith6@gmail.com>
2017-09-10 22:17:56 +0530

    init commit

--- selenium/src/test/java/mylog/selenium/LoggingTest.java
+++ selenium/src/test/java/mylog/selenium/LoggingTest.java
@@ -1,14 +1,23 @@
 package mylog.selenium;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class LoggingTest {
 
-	private static final Logger log = Logger.getLogger(LoggingTest.class);
-
-	public static void main(String[] args) {
-
-log.info("test");
+public static final Logger log = Logger.getLogger(LoggingTest.class);
+@BeforeTest
+public void initialize() {
+	
+}
+@Test
+public void runme() {
+	BasicConfigurator.configure();
+	PropertyConfigurator.configure("E:\\JavaProject\\selenium\\Properties\\log4j.properties ");
+		log.info("Test Message");
 	}
 
 }
