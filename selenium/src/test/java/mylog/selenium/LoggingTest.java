ed1ea99c8544e871e6ae4002c59aafc25d21085b
Zackery <Zackerysmith6@gmail.com>
2017-09-10 18:29:25 +0530

    initial Commit

---
+++ selenium/src/test/java/mylog/selenium/LoggingTest.java
@@ -0,0 +1,14 @@
+package mylog.selenium;
+
+import org.apache.log4j.Logger;
+
+public class LoggingTest {
+
+	private static final Logger log = Logger.getLogger(LoggingTest.class);
+
+	public static void main(String[] args) {
+
+log.info("test");
+	}
+
+}
