12e7e83141c16a3bd295f83c52df32a1051f0b91
Zackery <Zackerysmith6@gmail.com>
2017-09-12 01:29:40 +0530

    Final Auto test - Final

--- selenium/src/test/java/mylog/selenium/LoggingTest.java
+++ selenium/src/test/java/mylog/selenium/LoggingTest.java
@@ -18,7 +18,7 @@ public void runme() {
 	BasicConfigurator.configure();
 	PropertyConfigurator.configure("Properties/log4j.properties");
 		log.info("Update in Log");
-		System.out.println("Recheck");
+		System.out.println("Recheck - last");
 	}
 
 }
